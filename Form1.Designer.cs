7ea2084d8b3caebe75ee2c5f36bf04f53e60c072
bdavis <drynnbavis@gmail.com>
2016-11-14 15:55:08 -0500

    Working edition

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace OPCServer_Simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.ConnectedClients = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.SlikServer1 = new NDI.SLIKDA.Interop.SLIKServer();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pressTimer = new System.Windows.Forms.Timer(this.components);
+            this.GroupBox3 = new System.Windows.Forms.GroupBox();
+            this.measView = new System.Windows.Forms.ListView();
+            this.Index = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Measurement = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.LastRecorded = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.inchmm = new System.Windows.Forms.Button();
+            this.logDataBtn = new System.Windows.Forms.Button();
+            this.Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SlikServer1)).BeginInit();
+            this.GroupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Panel1
+            // 
+            this.Panel1.Controls.Add(this.ConnectedClients);
+            this.Panel1.Controls.Add(this.ExitButton);
+            this.Panel1.Controls.Add(this.Label1);
+            this.Panel1.Controls.Add(this.SlikServer1);
+            this.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Panel1.Location = new System.Drawing.Point(0, 332);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(307, 33);
+            this.Panel1.TabIndex = 10;
+            // 
+            // ConnectedClients
+            // 
+            this.ConnectedClients.AutoSize = true;
+            this.ConnectedClients.Location = new System.Drawing.Point(152, 11);
+            this.ConnectedClients.Name = "ConnectedClients";
+            this.ConnectedClients.Size = new System.Drawing.Size(13, 13);
+            this.ConnectedClients.TabIndex = 12;
+            this.ConnectedClients.Text = "0";
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButton.Location = new System.Drawing.Point(224, 6);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 9;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(3, 11);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(148, 13);
+            this.Label1.TabIndex = 6;
+            this.Label1.Text = "Number of Connected Clients:";
+            // 
+            // SlikServer1
+            // 
+            this.SlikServer1.Enabled = true;
+            this.SlikServer1.Location = new System.Drawing.Point(140, -159);
+            this.SlikServer1.Name = "SlikServer1";
+            this.SlikServer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("SlikServer1.OcxState")));
+            this.SlikServer1.Size = new System.Drawing.Size(32, 32);
+            this.SlikServer1.TabIndex = 11;
+            this.SlikServer1.OnClientConnect += new NDI.SLIKDA.Interop.SLIKServer.OnClientConnectEventHandler(this.SlikServer1_OnClientConnect);
+            this.SlikServer1.OnClientDisconnect += new NDI.SLIKDA.Interop.SLIKServer.OnClientDisconnectEventHandler(this.SlikServer1_OnClientDisconnect);
+            this.SlikServer1.OnRead += new NDI.SLIKDA.Interop.SLIKServer.OnReadEventHandler(this.SlikServer1_OnRead);
+            this.SlikServer1.OnWrite += new NDI.SLIKDA.Interop.SLIKServer.OnWriteEventHandler(this.SlikServer1_OnWrite);
+            // 
+            // pressTimer
+            // 
+            this.pressTimer.Interval = 50;
+            // 
+            // GroupBox3
+            // 
+            this.GroupBox3.Controls.Add(this.measView);
+            this.GroupBox3.Controls.Add(this.inchmm);
+            this.GroupBox3.Location = new System.Drawing.Point(12, 12);
+            this.GroupBox3.Name = "GroupBox3";
+            this.GroupBox3.Size = new System.Drawing.Size(284, 257);
+            this.GroupBox3.TabIndex = 12;
+            this.GroupBox3.TabStop = false;
+            this.GroupBox3.Text = "Measurement Data";
+            // 
+            // measView
+            // 
+            this.measView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Index,
+            this.Measurement,
+            this.LastRecorded});
+            this.measView.Location = new System.Drawing.Point(7, 39);
+            this.measView.Name = "measView";
+            this.measView.Size = new System.Drawing.Size(271, 212);
+            this.measView.TabIndex = 0;
+            this.measView.UseCompatibleStateImageBehavior = false;
+            this.measView.View = System.Windows.Forms.View.Details;
+            // 
+            // Index
+            // 
+            this.Index.Text = "Index";
+            this.Index.Width = 41;
+            // 
+            // Measurement
+            // 
+            this.Measurement.Text = "Measurement";
+            this.Measurement.Width = 77;
+            // 
+            // LastRecorded
+            // 
+            this.LastRecorded.Text = "Last Recorded Value";
+            this.LastRecorded.Width = 149;
+            // 
+            // inchmm
+            // 
+            this.inchmm.Location = new System.Drawing.Point(212, 10);
+            this.inchmm.Name = "inchmm";
+            this.inchmm.Size = new System.Drawing.Size(65, 23);
+            this.inchmm.TabIndex = 1;
+            this.inchmm.Text = "inch/mm";
+            this.inchmm.UseVisualStyleBackColor = true;
+            this.inchmm.Click += new System.EventHandler(this.inchmm_Click);
+            // 
+            // logDataBtn
+            // 
+            this.logDataBtn.Location = new System.Drawing.Point(18, 276);
+            this.logDataBtn.Name = "logDataBtn";
+            this.logDataBtn.Size = new System.Drawing.Size(271, 45);
+            this.logDataBtn.TabIndex = 13;
+            this.logDataBtn.Text = "Simulate Micrometer Click";
+            this.logDataBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 365);
+            this.Controls.Add(this.logDataBtn);
+            this.Controls.Add(this.GroupBox3);
+            this.Controls.Add(this.Panel1);
+            this.Icon = global::OPCServer_Simulator.Properties.Resources.icon;
+            this.Name = "Form1";
+            this.Text = "Gyptech Micrometer OPC Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Panel1.ResumeLayout(false);
+            this.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SlikServer1)).EndInit();
+            this.GroupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.Panel Panel1;
+        internal System.Windows.Forms.Button ExitButton;
+        internal System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Timer timer1;
+        private NDI.SLIKDA.Interop.SLIKServer SlikServer1;
+        private System.Windows.Forms.Label ConnectedClients;
+        private System.Windows.Forms.Timer pressTimer;
+        internal System.Windows.Forms.GroupBox GroupBox3;
+        private System.Windows.Forms.Button inchmm;
+        private System.Windows.Forms.ListView measView;
+        private System.Windows.Forms.ColumnHeader Index;
+        private System.Windows.Forms.ColumnHeader Measurement;
+        private System.Windows.Forms.ColumnHeader LastRecorded;
+        private System.Windows.Forms.Button logDataBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.VisualBasic;
+using NDI.SLIKDA.Interop;
+
+namespace OPCServer_Simulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            fillMeasView();
+        }
+
+        private ISLIKTags myOpcTags;
+        private Random randomizer = new Random();
+        private int itemIndex = 1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ***timer for log-data button created**
+            logDataBtn.Tag = new Stopwatch();
+            logDataBtn.MouseDown += new MouseEventHandler(button_MouseDown);
+            logDataBtn.MouseUp += new MouseEventHandler(button_MouseUp);
+            // create a new instance of an empty tag database
+            myOpcTags = SlikServer1.SLIKTags;
+
+            // define a simple read/write access-right, saves us OR'ing this
+            // enumeration multiple times
+            AccessPermissionsEnum readWriteAccess = AccessPermissionsEnum.sdaReadAccess | AccessPermissionsEnum.sdaWriteAccess;
+
+
+            // A Tag name is "just" a name. But when you prefix that name with a '.'
+            // then the OPC Server toolkit will automatically place that tag within
+            // a group. This simplifies the OPC Client experience when they browse
+            // your OPC Servers address-space.
+            // In this case, we will define a group: "USER"
+            // .... you will see it prefixed to the tagnames.
+
+            // add the user-tags. These are the tags that represent the 3 form
+            // controls on the LEFT side of the form
+            myOpcTags.Add("User.LogSet", (int)readWriteAccess, 0, 0, DateTime.Now, null);
+            myOpcTags["User.LogSet"].DataType = (short)VariantType.Boolean;
+            myOpcTags["User.LogSet"].SetVQT(Convert.ToInt32(true), 192, DateTime.Now);
+            myOpcTags.Add("User.meas1", (int)readWriteAccess, 0, 0, DateTime.Now, null);
+            myOpcTags["User.meas1"].DataType = (short)VariantType.Double;
+            myOpcTags["User.meas1"].SetVQT((0.0000), 192, DateTime.Now);
+            myOpcTags.Add("User.meas2", (int)readWriteAccess, 0, 0, DateTime.Now, null);
+            myOpcTags["User.meas2"].DataType = (short)VariantType.Double;
+            myOpcTags["User.meas2"].SetVQT((0.0000), 192, DateTime.Now);
+            myOpcTags.Add("User.meas3", (int)readWriteAccess, 0, 0, DateTime.Now, null);
+            myOpcTags["User.meas3"].DataType = (short)VariantType.Double;
+            myOpcTags["User.meas3"].SetVQT((0.0000), 192, DateTime.Now);
+            myOpcTags.Add("User.meas4", (int)readWriteAccess, 0, 0, DateTime.Now, null);
+            myOpcTags["User.meas4"].DataType = (short)VariantType.Double;
+            myOpcTags["User.meas4"].SetVQT((0.0000), 192, DateTime.Now);
+            myOpcTags.Add("User.meas5", (int)readWriteAccess, 0, 0, DateTime.Now, null);
+            myOpcTags["User.meas5"].DataType = (short)VariantType.Double;
+            myOpcTags["User.meas5"].SetVQT((0.0000), 192, DateTime.Now);
+            myOpcTags.Add("User.meas6", (int)readWriteAccess, 0, 0, DateTime.Now, null);
+            myOpcTags["User.meas6"].DataType = (short)VariantType.Double;
+            myOpcTags["User.meas6"].SetVQT((0.0000), 192, DateTime.Now);
+            myOpcTags.Add("User.meas7", (int)readWriteAccess, 0, 0, DateTime.Now, null);
+            myOpcTags["User.meas7"].DataType = (short)VariantType.Double;
+            myOpcTags["User.meas7"].SetVQT((0.0000), 192, DateTime.Now);
+            myOpcTags.Add("User.meas8", (int)readWriteAccess, 0, 0, DateTime.Now, null);
+            myOpcTags["User.meas8"].DataType = (short)VariantType.Double;
+            myOpcTags["User.meas8"].SetVQT((0.0000), 192, DateTime.Now);
+            myOpcTags.Add("User.meas9", (int)readWriteAccess, 0, 0, DateTime.Now, null);
+            myOpcTags["User.meas9"].DataType = (short)VariantType.Double;
+            myOpcTags["User.meas9"].SetVQT((0.0000), 192, DateTime.Now);
+            myOpcTags.Add("User.meas10", (int)readWriteAccess, 0, 0, DateTime.Now, null);
+            myOpcTags["User.meas10"].DataType = (short)VariantType.Double;
+            myOpcTags["User.meas10"].SetVQT((0.0000), 192, DateTime.Now);
+
+            // In this example, we will also register and start the server
+            // Under normal circumstances you would NOT do this, because this is a
+            // step that should be done once and once-only, at the end of an
+            // installation routine. Most OPC Server implementations allow the use
+            // of a command-line argument to call this function.
+
+            SlikServer1.RegisterServer();
+
+            // Now START the OPC Server interface. This is called whenever your
+            // application starts.
+            SlikServer1.StartServer();
+        }
+
+        private void fillMeasView()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                ListViewItem lvi = new ListViewItem(i.ToString());
+                lvi.SubItems.Add("null");
+                lvi.SubItems.Add("null");
+                measView.Items.Add(lvi);
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            if ((MessageBox.Show("Close the OPC Server and Exit?", "Exit?", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == System.Windows.Forms.DialogResult.Yes))
+            {
+                try
+                {
+                    // Notify the OPC Clients that you want to shut down. This gives
+                    // each OPC Client enough time to gracefully disconnect.
+                    SlikServer1.RequestDisconnect("User Requested Shutdown");
+
+                    // This function, just like the ".RegisterServer" as seen in the 
+                    // Form_Load() event is a function that should not be called under
+                    // normal circumstances. This should be done by the installer
+                    // when the application is being removed from the computer.
+                    //SlikServer1.UnregisterServer()
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
+
+        }
+
+        private void SlikServer1_OnClientConnect(object sender, SLIKServer.OnClientConnectEventArgs eventArgs)
+        {
+            ConnectedClients.Text = eventArgs.NumClients.ToString();
+        }
+
+        private void SlikServer1_OnClientDisconnect(object sender, SLIKServer.OnClientDisconnectEventArgs eventArgs)
+        {
+            ConnectedClients.Text = eventArgs.NumClients.ToString();
+        }
+
+        private void SlikServer1_OnWrite(object sender, SLIKServer.OnWriteEventArgs eventArgs)
+        {
+            for (int i = 0; i <= (eventArgs.Count - 1); i++)
+            {
+                // Get the "next" item in the list
+                ISLIKTag currentItem = eventArgs.Tags[i];
+                object currentValue = eventArgs.Values[i];
+
+                // We will look at the name of the OPC Tag to then figure out WHERE
+                // to SEND the data to....
+
+                // Specify that the Item at *this* position is NOT in error!
+                eventArgs.Errors[i] = (int)OPCDAErrorsEnum.sdaSOK;
+            }
+
+            // Now specify that we completed this event successfully
+            eventArgs.Result = (int)OPCDAErrorsEnum.sdaSOK;
+
+        }
+
+        private void SlikServer1_OnRead(object sender, SLIKServer.OnReadEventArgs eventArgs)
+        {
+            //
+            // Iterate thru each/every item that the OPC Client has requested us to READ
+
+            {
+
+                for (int i = 0; i <= (eventArgs.Count - 1); i++)
+                {
+                    // Get the "next" item in the list
+                    ISLIKTag currentItem = eventArgs.Tags[i];
+
+                    // Now to return the value of this tag, along with
+                    // GOOD quality, and a current timestamp
+                    //   (Quality codes are: 192=GOOD; 0=BAD)
+                    //
+                    // We will look at the name of the OPC Tag to then figure out WHERE
+                    // to get the data from....
+
+                    switch ((currentItem.Name))
+                    {
+                        // Checkbox on the LEFT side of the form
+                        case "User.LogSet":
+                            eventArgs.Tags[i].SetVQT(true, 192, DateTime.Now);
+
+                            break;
+                        default:
+                            eventArgs.Tags[i].SetVQT(Convert.ToDouble(100), 192, DateTime.Now);
+
+                            break;
+                    }
+
+
+                    // Specify that the Item at *this* position is NOT in error!
+                    eventArgs.Errors[i] = (int)OPCDAErrorsEnum.sdaSOK;
+                }
+
+                // Now specify that we completed this event successfully
+                eventArgs.Result = (int)OPCDAErrorsEnum.sdaSOK;
+
+            }
+
+
+        }
+
+        private void swapUnits() //not sure if I want this feature
+        {
+            foreach (ListViewItem item in measView.Items)
+            {
+                if (item.SubItems[1].Text.Contains("inch"))
+                {
+                    string[] splitItem = item.SubItems[1].Text.Split(' ');
+                    double value = Convert.ToDouble(splitItem[0]);
+                    value = value * 25.4;
+                    item.SubItems[1].Text = value.ToString("0.0000") + " mm";
+                }
+                else if (item.SubItems[1].Text.Contains("mm"))
+                {
+                    string[] splitItem = item.SubItems[1].Text.Split(' ');
+                    double value = Convert.ToDouble(splitItem[0]);
+                    value = value / 25.4;
+                    item.SubItems[1].Text = value.ToString("0.0000") + " inch";
+
+                }
+                else
+                    continue;
+            }
+        }
+
+        private void inchmm_Click(object sender, EventArgs e)
+        {
+            //swapUnits();
+        }
+
+        private void simulateDataEntry()
+        {
+            double value = randomizer.Next(8000, 12000);
+            value = value / 10000;
+            measView.Items[itemIndex - 1].SubItems[1].Text = value.ToString("0.0000") + " inch";
+            if (myOpcTags != null)
+            {
+                myOpcTags["User.meas" + itemIndex].SetVQT(Convert.ToDouble(value), 192, DateTime.Now);
+            }
+        }
+
+        void button_MouseUp(object sender, MouseEventArgs e)
+        {
+            Stopwatch watch = ((sender as Button).Tag as Stopwatch);
+            watch.Stop();
+            simulateDataEntry();
+            if (watch.Elapsed.TotalMilliseconds > 500)
+            {
+                itemIndex = 1;
+                MessageBox.Show("Button pressed for longer than 500, data log being saved by client. Index reset to 1.");
+                if (myOpcTags != null)
+                    myOpcTags["User.LogSet"].SetVQT(true, 192, DateTime.Now); //this bit is toggled here to tell the client to save the set
+                foreach (ListViewItem item in measView.Items)
+                {
+                    item.SubItems[2].Text = item.SubItems[1].Text;
+                    item.SubItems[2].BackColor = Color.Gray;
+                    item.SubItems[1].Text = "null";
+                }
+                myOpcTags["User.meas1"].SetVQT((0.0000), 192, DateTime.Now);
+                myOpcTags["User.meas2"].SetVQT((0.0000), 192, DateTime.Now);
+                myOpcTags["User.meas3"].SetVQT((0.0000), 192, DateTime.Now);
+                myOpcTags["User.meas4"].SetVQT((0.0000), 192, DateTime.Now);
+                myOpcTags["User.meas5"].SetVQT((0.0000), 192, DateTime.Now);
+                myOpcTags["User.meas6"].SetVQT((0.0000), 192, DateTime.Now);
+                myOpcTags["User.meas7"].SetVQT((0.0000), 192, DateTime.Now);
+                myOpcTags["User.meas8"].SetVQT((0.0000), 192, DateTime.Now);
+                myOpcTags["User.meas9"].SetVQT((0.0000), 192, DateTime.Now);
+                myOpcTags["User.meas10"].SetVQT((0.0000), 192, DateTime.Now);
+            }
+            else if (itemIndex == 10)
+            {
+                itemIndex = 10;
+                if (myOpcTags != null)
+                    myOpcTags["User.LogSet"].SetVQT(false, 192, DateTime.Now);
+            }
+            else
+            {
+                itemIndex++;
+                if (myOpcTags != null)
+                    myOpcTags["User.LogSet"].SetVQT(false, 192, DateTime.Now);
+            }
+
+            watch.Reset();
+
+        }
+
+        void button_MouseDown(object sender, MouseEventArgs e)
+        {
+            ((sender as Button).Tag as Stopwatch).Start();
+        }
+    }
+}
